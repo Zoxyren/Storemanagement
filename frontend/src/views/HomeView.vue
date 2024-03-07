c419638614e7bc4a64a0e21ecaa0acf7e08116c1
Oliver <wallisch.o@freenet.de>
2024-03-07 14:38:14 +0100

    implemented bootsrap

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <b-row>
+      <b-col>
+        <b-card title="Welcome to Vue.js with Bootstrap">
+          <p>This is a sample component demonstrating how to integrate Bootstrap with Vue.js.</p>
+          <b-button variant="primary">Click me</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'SampleComponent'
+};
+</script>
